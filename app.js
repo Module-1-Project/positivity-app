fe25b75214233fd650d151aaf3d5ae2aabbe0c36
austinbhan <austinbhan@outlook.com>
2022-05-31 17:45:19 -0700

    Loading Quotes in Home Page

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { fetchQuote } from './fetch-utils.js';
+import { renderQuote } from './render-utils.js';
+
 const signInSignUp = document.getElementById('auth');
 // const goToProfile = document.getElementById('go-to-profile');
 const aboutCreators = document.getElementById('about-button');
@@ -9,3 +12,20 @@ aboutCreators.addEventListener('click', () => {
 signInSignUp.addEventListener('click', () => {
     window.location.href = './login-page';
 });
+
+
+async function onLoad() {
+    const quoteRender = document.getElementById('quote-render');
+    const quoteEl = await fetchQuote();
+    console.log(quoteEl);
+    
+    for (let quotes of quoteEl) {
+        const p = document.createElement('p');
+        p.value = quotes.id;
+        p.textContent = quotes.quote;
+        quoteRender.append(p);
+    }
+}
+
+onLoad();
+
