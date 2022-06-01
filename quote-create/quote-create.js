07020336f993ee9b21e6314daa84040386c293d1
austinbhan <austinbhan@outlook.com>
2022-06-01 11:08:40 -0700

    Logout button in quote-create.js

--- quote-create/quote-create.js
+++ quote-create/quote-create.js
@@ -1,3 +1,5 @@
+import { logout } from '../fetch-utils.js';
+
 const homeButton = document.getElementById('home');
 const logOutButton = document.getElementById('logout');
 const submitButton = document.getElementById('journal-submit');
@@ -7,6 +9,10 @@ const aboutButton = document.getElementById('about-button');
 
 homeButton.addEventListener('click', () => {
     window.location.href = '../index.html';
+});
+
+logOutButton.addEventListener('click', () => {
+    logout();
 });
 
 console.log('This button is working');
